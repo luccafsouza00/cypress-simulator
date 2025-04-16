a47c1f83093034134a2944141d7137c86c253f4b
Lucca Souza <luccafsouza00@gmail.com>
2025-04-16 00:31:57 -0300

    add solutions until exercise 15

--- cypress/e2e/cypress-simulator.cy.js
+++ cypress/e2e/cypress-simulator.cy.js
@@ -66,11 +66,9 @@ describe("Cypress simulator", () => {
     );
 
     cy.get(".expand-collapse").click();
-
     cy.get("#collapseIcon").should("be.visible");
 
     cy.get(".expand-collapse").click();
-
     cy.get("#expandIcon").should("be.visible");
   });
 
@@ -81,30 +79,119 @@ describe("Cypress simulator", () => {
     cy.contains("button", "Login").should("be.visible");
   });
 
-  it.only("shows and hides the logout button", () => {
+  it("shows and hides the logout button", () => {
     cy.get("#sandwich-menu").click();
-
     cy.get("#logoutButton").should("be.visible");
 
     cy.get("#sandwich-menu").click();
-    
     cy.get("#logoutButton").should("not.be.visible");
   });
 
-  // it("comando help");
+  it("shows the running state before showing the final result", () => {
+    cy.fillInputAndRun("test");
+    cy.get("#runButton")
+      .should("contain.text", "Running...")
+      .and("be.disabled");
 
-  // // Redirecionamentos
-  // it("Deve redirecionar corretamente os links exibidos no output");
+    cy.validateOutputText("Running...", "Please wait.");
+    cy.validateOutputText("Error", "test");
+  });
 
-  // it("Deve redirecionar corretamente ao clicar em links da página");
+  it("checks the run button disabled and enabled states", () => {
+    cy.get("#runButton").should("be.disabled");
+    cy.get("#codeInput").type("t");
 
-  // // Cookies
-  // it("Deve armazenar os cookies corretamente após o login");
+    cy.get("#runButton").should("be.enabled");
+    cy.get("#codeInput").clear();
 
-  // it("Deve limpar os cookies corretamente após o logout");
+    cy.get("#runButton").should("be.disabled");
+  });
 
-  // // Logout
-  // it("Deve realizar o logout e redirecionar para a tela de login");
+  it("clears the code input when logging off then logging in again", () => {
+    cy.get("#codeInput").type("t");
 
-  // it("running state");
+    cy.get("#sandwich-menu").click();
+    cy.get("#logoutButton").click();
+    cy.contains("button", "Login").click();
+
+    cy.get("#codeInput").should("have.value", "");
+  });
+
+  it("disables the run button when logging off then logging in again", () => {
+    cy.get("#codeInput").type("t");
+
+    cy.get("#sandwich-menu").click();
+    cy.get("#logoutButton").click();
+    cy.contains("button", "Login").click();
+
+    cy.get("#runButton").should("be.disabled");
+  });
+
+  it.only("clears the code output when logging off then logging in again", () => {
+    cy.fillInputAndRun("test")
+    cy.validateOutputText("Error", "test")
+
+    cy.get("#sandwich-menu").click();
+    cy.get("#logoutButton").click();
+    cy.contains("button", "Login").click();
+
+    cy.get("#outputArea").should("have.text", "")
+  });
+});
+
+describe("Cypress Simulator - Cookies consent", () => {
+  beforeEach(() => {
+    cy.visit("./src/index.html?skipCaptcha=true");
+    cy.contains("button", "Login").click();
+  });
+
+  it("consents on the cookies usage", () => {
+    cy.get("#acceptCookies").as("acceptCookiesButton").click();
+
+    cy.get("@acceptCookiesButton").should("not.be.visible");
+    cy.window()
+      .its("localStorage.cookieConsent")
+      .should("be.equal", "accepted");
+    // cy.getAllLocalStorage().then(localstorage => {
+    //   expect(localstorage['http://localhost:52570']['cookieConsent']).to.eql('accepted')
+    // })
+  });
+
+  it("declines on the cookies usage", () => {
+    cy.get("#declineCookies").as("declineCookiesButton").click();
+
+    cy.get("@declineCookiesButton").should("not.be.visible");
+    cy.window()
+      .its("localStorage.cookieConsent")
+      .should("be.equal", "declined");
+  });
+});
+
+describe("Cypress Simulator - Captcha", () => {
+  beforeEach(() => {
+    cy.visit("./src/index.html?");
+    cy.contains("button", "Login").click();
+  });
+
+  it("disables the captcha verify button when no answer is provided or it's cleared", () => {
+    cy.get("#verifyCaptcha").should("be.disabled");
+
+    cy.get("#captchaInput").type("1");
+    cy.get("#verifyCaptcha").should("be.enabled");
+
+    cy.get("#captchaInput").clear();
+    cy.get("#verifyCaptcha").should("be.disabled");
+  });
+
+  it("shows an error on a wrong captcha answer and goes back to its initial state", () => {
+    cy.get("#captchaInput").type("21");
+    cy.get("#verifyCaptcha").click();
+
+    cy.contains(".error", "Incorrect answer, please try again.").should(
+      "be.visible"
+    );
+
+    cy.get("#captchaInput").should("have.value", "");
+    cy.get("#verifyCaptcha").should("be.disabled");
+  });
 });
